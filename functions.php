7a2167903f023ebe1eabfa4aae568c5ec88a4e33
tsquez <tsquez@gmail.com>
2014-03-06 21:33:24 -0700

    5.2.0
    Changed version number to coincide with the update of WP-Forge to
    Foundation 5.2.0

--- functions.php
+++ functions.php
@@ -1,4 +1,27 @@
 <?php
-// Add your theme specific functions here
+/**
+ * WP-Starter functions and definitions
+ *
+ * Sets up the theme and provides some helper functions, which are used
+ * in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters, @link http://codex.wordpress.org/Plugin_API
+ *
+ * @package WordPress
+ * @subpackage WP_Starter
+ * @since WP-Starter 5.2.0
+ */
+
+// Start adding your theme specific functions below
 
 ?>
